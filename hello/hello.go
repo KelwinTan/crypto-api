6a18a751ca1dec611065d527d38cba4e980cc573
kelwintan <kelwin.tantono@binus.ac.id>
2021-04-23 16:27:22 +0700

    second module

--- hello/hello.go
+++ hello/hello.go
@@ -3,13 +3,29 @@ package main
 import (
 	"fmt"
 	"crypto.com/greetings"
+	"log"
 )
 
 // import "rsc.io/quote"
 
 func main(){
 	// fmt.Println("Hello World!")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	//request a greeting message
+	// message, err := greetings.Hello("Gladys")
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
 	// fmt.Println(quote.Opt())
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	// message := greetings.Hello("Gladys")
+	// for _, message := range messages{
+	// 	// message := messages
+	// 	fmt.Println(message)
+	// }
+	fmt.Println(messages)
 }
